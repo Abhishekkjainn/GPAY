0262cc4d718f3ab5c71c0d7fe3b8e060c1e14e11
abhishekkjainn <jainabhishek1904@gmail.com>
2023-09-24 20:57:28 +0530

    final commit

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ function savedata() {
   let paytomain = document.getElementById('payingtomain').value;
   let upiid = document.getElementById('upimain').value;
   let trnsid = document.getElementById('upitransactionmain').value;
-  localStorage.setItem('mainamount', mainamount);
-  localStorage.setItem('paytomain', paytomain);
-  localStorage.setItem('upiid', upiid);
-  localStorage.setItem('trnsid', trnsid);
+  if (mainamount == '' && paytomain == '' && upiid == '' && trnsid == '') {
+    console.log('nothing to show');
+  } else {
+    localStorage.setItem('mainamount', mainamount);
+    localStorage.setItem('paytomain', paytomain);
+    localStorage.setItem('upiid', upiid);
+    localStorage.setItem('trnsid', trnsid);
+  }
 }
 
 function topaidpage() {
